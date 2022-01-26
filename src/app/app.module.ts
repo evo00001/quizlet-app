b38b8e626341549054603c969a83d4c89d90542c
evo00001 <evo00001@mix.wvu.edu>
2022-01-26 08:49:18 -0500

    Partially Completed top nav bar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './navigation/footer/footer.component';
 import { TopNavBarComponent } from './navigation/topnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
