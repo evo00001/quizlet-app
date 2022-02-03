11c122ea6e237f3d7e549d4d989e9f6147e394e0
evo00001 <evo00001@mix.wvu.edu>
2022-02-02 21:10:32 -0500

    Set up and implementation of card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './navigation/footer/footer.component';
 import { TopNavBarComponent } from './navigation/topnavbar.component';
+import { CategoryCardComponent } from './store/category.card';
 import { CategoryCarouselComponent } from './store/category.carousel';
 
 @NgModule({
@@ -11,7 +12,8 @@ import { CategoryCarouselComponent } from './store/category.carousel';
     AppComponent,
     TopNavBarComponent,
     FooterComponent,
-    CategoryCarouselComponent
+    CategoryCarouselComponent,
+    CategoryCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/store/category.card.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'quiz-category-card',
+    templateUrl: 'category.card.html',
+    styleUrls: ['category.card.css']
+})
+export class CategoryCardComponent{
+
+}
